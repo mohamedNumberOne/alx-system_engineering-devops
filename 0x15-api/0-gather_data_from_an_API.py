d9bcaeef56aa47b91e074f137d855ff93efed9df
mohamedNumberOne <79853513+mohamedNumberOne@users.noreply.github.com>
2024-07-29 23:04:18 +0200

    Update 0-gather_data_from_an_API.py

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -7,28 +7,33 @@ import requests
 import sys
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
+    if len(sys.argv) != 2:
         print("Usage: {} <employee_id>".format(sys.argv[0]))
         sys.exit(1)
 
-    userId = sys.argv[1]
     try:
-        user = requests.get("https://jsonplaceholder.typicode.com/users/{}"
-                            .format(userId)).json()
-        name = user.get('name')
-
-        todos = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'
-                             .format(userId)).json()
-        totalTasks = len(todos)
-        completedTasks = [task for task in todos if task.get('completed')]
-
-        print('Employee {} is done with tasks({}/{}):'
-              .format(name, len(completedTasks, totalTasks)))
-
-        for task in completedTasks:
-            print('\t {}'.format(task.get('title')))
-
-    except requests.exceptions.RequestException as e:
-        print("HTTP Request failed: {}".format(e))
+        user_id = int(sys.argv[1])
     except ValueError:
-        print("Invalid user ID")
+        print("Employee ID must be an integer")
+        sys.exit(1)
+
+    user_url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    todos_url = f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
+
+    try:
+        user = requests.get(user_url).json()
+        todos = requests.get(todos_url).json()
+
+        employee_name = user.get('name')
+        total_tasks = len(todos)
+        completed_tasks = [task for task in todos if task.get('completed')]
+
+        print("Employee {} is done with tasks({}/{}):".format(
+            employee_name, len(completed_tasks), total_tasks))
+
+        for task in completed_tasks:
+            print("\t {}".format(task.get('title')))
+
+    except requests.RequestException as e:
+        print(f"HTTP Request failed: {e}")
+        sys.exit(1)
